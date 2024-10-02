2623a3d60676eb21711ce131aac6c087f885af67
Ajoykrishna Nath <ajoy123krishna@gmail.com>
2024-10-02 20:24:37 +0530

    Update code.java
    Adding ArrayList to easily add or remove items from library

--- code.java
+++ code.java
@@ -3,24 +3,31 @@ You have to implement a library using Java Class Library
 Methods: addBook, issueBook, returnBook, showAvailableBooks
 Properties: Array to store the available books
  */
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
 
 class Library{
     Scanner sc = new Scanner(System.in);
-    String []books={"B20315H","N564A","DO97GK","BLR459I","UHB780"};
-    public void  addBook(String s){
+    ArrayList<String> books= new ArrayList<>(Arrays.asList("B20315H","N564A","DO97GK","BLR459I","UHB780"));
 
+    public void  addBook(){
+        System.out.println("Enter the serial no. of the new book you want to add to library:");
+        books.add(sc.nextLine());
     }
     public void issueBook(){
         System.out.println("Enter the serial no. of the book you want to issue:");
         String s= sc.nextLine();
-        addBook(s);
+        books.remove(s);
     }
     public void returnBook(){
-
+        System.out.println("Enter the serial no. of the book you want to return:");
+        String r= sc.nextLine();
+        books.add(r);
     }
     public void showAvailableBooks(){
-
+        System.out.println("Serial no. of Availbale books are: "+books);
     }
 }
 public class code {
